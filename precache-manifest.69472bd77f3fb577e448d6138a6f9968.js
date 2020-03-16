373a1fca1914af2320211f3e9f4befe65e5788e7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-16 08:08:58 +0000

    Deploy sachin9890/assignment_zimbra to github.com/sachin9890/assignment_zimbra.git:gh-pages

---
+++ precache-manifest.69472bd77f3fb577e448d6138a6f9968.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d7c961f34d5061f3ea0d0b37185574",
+    "url": "/index.html"
+  },
+  {
+    "revision": "729a3c71378cbf8cbf7a",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d08fdee424fd3d2be008",
+    "url": "/static/css/main.96c8dd64.chunk.css"
+  },
+  {
+    "revision": "729a3c71378cbf8cbf7a",
+    "url": "/static/js/2.9371fc63.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.9371fc63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08fdee424fd3d2be008",
+    "url": "/static/js/main.aea1f887.chunk.js"
+  },
+  {
+    "revision": "ca37bca7cff2320117cd",
+    "url": "/static/js/runtime-main.cb7cc27c.js"
+  }
+]);
